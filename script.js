b32b8077e994dc11a3d5796cc6f336f4f768324c
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-22 11:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let level = 0;
+
+        const ele = document.getElementById("level")
+
+        let curr = ele;
+        while(curr){
+            level++;
+            curr = curr.parentElement;
+        }
+
+        alert("The level of the element is: " + level)
